da8e17a7a8dbfd8ad7cbb1ecb7bfb632fbfe88d5
VelardeSoft <u20211c221@upc.edu.pe>
2024-05-12 22:47:38 -0500

    feat: Corrección y mejora del código de versión anterior.

--- nodos.py
+++ nodos.py
@@ -1,26 +1,25 @@
-import folium # Importamos la librería folium
-import pandas as pd # Importamos la librería pandas
+import pandas as pd
+import networkx as nx
+import folium
 
-archivo = pd.read_csv("crimenes_febrero_2024.csv")
+data = pd.read_csv('crimenes_febrero_2024.csv')
 
-mapa = folium.Map(location=[34.0522, -118.2437], zoom_start = 11)  # Locations
+G = nx.Graph() # Grafo vacio
 
-for index, row in archivo.iterrows():
-    popup_content = f"""
-    <h5>-------  DELLATE DEL CRIMEN  -------</h5>
-    <p> Area: {row['AREA-NAME']}</p>
-    <p> Descripción: {row['Crm Cd Desc']}</p>
-    <p> Fecha del crimen: {row['DATE OCC']}</p>
-    <p> Hora del crimen: {row['TIME OCC']}</p>
-    <p> Ubicación: {row['LOCATION']}</p>
-    <h5> -------  DATOS DEL VICTIMA  -------</h5>
-    <p> Edad: {row['Vict Age']} años</p>
-    <p> Sexo: {row['Vict Sex']}</p>
-    <p> Estado del caso: {row['Status']}</p>
-    """
-    folium.Marker(
-        location=[row['LAT'], row['LON']],
-        popup=folium.Popup(html=popup_content, max_width=2650),
+for index, row in data.iterrows():
+    G.add_node(row['AREA-NAME'], pos=(row['LAT'], row['LON']))
+
+mapa = folium.Map(location=[34.0522, -118.2437], zoom_start=11) #Visualización
+
+for node in G.nodes:
+    lat, lon = G.nodes[node]['pos']
+    folium.CircleMarker(
+        location=[lat, lon],
+        radius=8, color='blue', fill_color='red',
+        fill=True,
+        fill_opacity=1,  # Opacidad del relleno (0-1) 0 trasn, 1 opaco
+        popup=node  # Muestra el nombre del área como popup
     ).add_to(mapa)
 
-mapa.save('img/mapas.html')
+mapa.save('mapa_crímenes.html')
+
